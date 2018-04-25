ed1d44b5407f98f766eb077f4443549118a3b312
Jonathan Tiede <skysoldiermars@gmail.com>
2018-04-25 19:11:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,11 +2,11 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-  get "/newteam" do 
-    erb :newteam 
+  get "/newteam" do
+    erb :newteam
   end
 
-  post "/team" do 
+  post "/team" do
     erb :team
   end
 
